54578a7674678106b06fed8a991338b45535fd08
ali sameer <alisameerawad123@gmail.com>
2025-09-29 22:41:15 -0500

    adding account class

--- Account.java
+++ Account.java
@@ -1,6 +1,44 @@
-public class Account {
-    public static void main(String[] args) {
 
-        System.out.println("Banking System Starting...");
+import java.util.Set;
+
+public class Account {
+
+    private String ownerName;
+    private String accountNumber;
+    private String pinNumber;
+    private double balance;
+
+    public Account(String accountNumber, String ownerName, String pinNumber, double initialBalance) {
+        this.accountNumber = accountNumber;
+        this.ownerName = ownerName;
+        this.pinNumber = pinNumber;
+        this.balance = initialBalance;
+    }
+    public double getBalance() {
+        return this.balance;
+    }
+    public void deposit(double amount) {
+        if(amount > 0) {
+            this.balance += amount;
+        } else {
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+    public void withdraw(double amount) {
+        if(amount > 0 && amount <= this.balance) {
+            this.balance -= amount;
+        } else {
+            System.out.println("Invalid withdrawal amount.");
+        }
+    }
+    public boolean verifyPin(String enterdPin){
+        if(this.pinNumber.equals(enterdPin)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public String getAccountNumber() {
+        return this.accountNumber;
     }
 }
